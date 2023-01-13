7234db48779dcf4009a6b3974e9851ef022419a5
SoumyaCode74 <100553212+SoumyaCode74@users.noreply.github.com>
2023-01-13 12:21:14 +0530

    Practice during udemy session

---
+++ solutions/variables/src/main.rs
@@ -0,0 +1,6 @@
+use variables::*;   //:: is the reference operator, same function as . operator in C or Python
+fn main() {
+    let x = 0x0001_1200;
+    println!("Hello, world! {}",x);
+    greet();
+}
